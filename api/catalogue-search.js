bb768df64969a08e634b8e668384cd9880d5fcaf
Jobbo7 <kjobson7@outlook.com>
2026-08-27 16:43:39 +1000

    Create catalogue-search.js

---
+++ api/catalogue-search.js
@@ -0,0 +1,22 @@
+{
+  success: true,
+  source: "autoinfo",
+
+  vehicle: {
+    make: "TOYOTA",
+    model: "HIACE",
+    year: 2014
+  },
+
+  category: "oil_filter",
+
+  parts: [
+    {
+      brand: "RYCO",
+      partNumber: "...",
+      oemNumbers: [],
+      description: "...",
+      fitmentVerified: true
+    }
+  ]
+}
